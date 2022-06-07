e61125db8b2ee0724bec1be3d0409429da8ddb81
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-06-07 19:15:28 +0500

    Add authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'pages/index'
   root to: 'pages#index'
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
