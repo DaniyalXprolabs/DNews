9a568e480324ab16cbff1ae728f320c47e4283b2
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-06-08 13:51:58 +0500

    Change the Navbar and make it responsive

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   devise_for :users, :controllers =>{:registrations => "registrations"}
   get 'pages/index'
-  root to: 'pages#index'
+  root to: 'posts#index'
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
+    resources :posts do
+      collection do
+        get 'hobby'
+        get 'study'
+        get 'team'
+      end
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
